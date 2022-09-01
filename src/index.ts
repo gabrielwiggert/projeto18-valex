8caa6ab081bb24ccf7b5e0cd6b47bef5b9cb91b6
Gabriel Wiggert <gabrielwiggert@gmail.com>
2022-09-01 19:20:11 -0300

    database created

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(router);
 app.use(errorHandler);
 
-const PORT = Number(process.env.PORT) || 5000;
+const PORT = Number(process.env.PORT) || 5009;
 
 app.listen(PORT, () => {
   console.log(`Server com TS rodando na porta: ${PORT}`);
